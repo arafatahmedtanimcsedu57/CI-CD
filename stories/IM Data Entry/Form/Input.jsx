44cb62b9da85eaae83ed690e45338e522a630f92
arafatgitlab <arafatgitlab.gmail.com>
2020-06-07 10:59:06 +0600

    IM Data Entry
    Form
    
    Input

--- stories/IM Data Entry/Form/Input.jsx
+++ stories/IM Data Entry/Form/Input.jsx
@@ -4,9 +4,8 @@ import './style/Input.css';
 
 class Input extends Component {
     state = {
-        inputData: {
-
-        }
+        inputType1: null,
+        inputType2: null
     }
     render() {
         return (
@@ -14,10 +13,19 @@ class Input extends Component {
                 <section className="example">
                     <h3 className="ex-title">Input Type: 1(Text)</h3>
                     <div className="group">
-                        <input type="text" required="required" />
+                        <input
+                            onChange={(e) => this.setState({
+                                inputType1: e.target.value
+                            })}
+                            type="text"
+                            required="required"
+                        />
                         <span className="highlight"></span>
                         <span className="bar"></span>
                         <label>Name</label>
+
+
+                        <span>{this.state.inputType1}</span>
                     </div>
                 </section>
 
@@ -25,10 +33,20 @@ class Input extends Component {
                 <section className="example">
                     <h3 className="ex-title">Input Type: 2(Text Area)</h3>
                     <div className="group">
-                        <textarea type="textarea" rows="5" required="required"></textarea>
+                        <textarea
+                            onChange={(e) => this.setState({
+                                inputType2: e.target.value
+                            })}
+                            type="textarea"
+                            rows="5"
+                            required="required"
+                        />
                         <span className="highlight"></span>
                         <span className="bar"></span>
                         <label>Message</label>
+
+
+                        <span>{this.state.inputType2}</span>
                     </div>
                 </section>
             </div>
